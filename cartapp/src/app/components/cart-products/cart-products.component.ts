0ffa0b2bc00771d177ec0df93d4a85f434a1737b
cdaitha <cdaitha@primesoft.net>
2021-01-03 15:28:39 +0530

    Final phase done

--- cartapp/src/app/components/cart-products/cart-products.component.ts
+++ cartapp/src/app/components/cart-products/cart-products.component.ts
@@ -10,6 +10,7 @@ export class CartProductsComponent implements OnInit {
 
   products:any;
   prices:any = [];
+  cartArray:any = [];
   finalCount:any = 0;
   finalPrice:any = 0;
   cartData:any = {count: 0, price: 0};
@@ -22,24 +23,35 @@ export class CartProductsComponent implements OnInit {
        this.prices = this.products.reduce((acc:any, product: any) => {
          return {...acc, [product.id]: { price: product.price, count: 0}}
       }, {})
+      this.cartArray = Object.keys(this.prices).map(key => {
+        return (Object.assign({},this.prices[key]));
+      });
     })
   }
 
   updateCart(event:any) {
     const { count, id } = event;
-    this.products.forEach((element:any) => {
+   
+    this.products.forEach((element:any, key:any) => {
       if (element.id == id && count > 0) {
-        this.prices[id] = { price: element.price * count, count };
-      }
+        this.prices[id] = { price: element.price * count, count, id };
+        this.cartArray[key] = { price: element.price * count, count, id };
+      } else this.cartArray[key] = { ...this.cartArray[key]}
     });
     
-    let cartArray = Object.keys(this.prices).map(key => (this.prices[key]));
-    this.finalCount = cartArray.reduce((accumulator:any, currentValue:any) => {
+    this.cartArray.forEach((element:any) => {
+      if (element.id == id && count == 0) {
+        element.price = element.price * count;
+        element.count = count;
+      }
+    })
+
+    this.finalCount = this.cartArray.reduce((accumulator:any, currentValue:any) => {
       return ({ count: accumulator.count + currentValue.count})
     });
-    this.finalPrice = cartArray.filter((val) => val.count > 0 ).reduce((acc:any, curr: any) => {
+    this.finalPrice = this.cartArray.filter((val:any) => val.count > 0 ).reduce((acc:any, curr: any) => {
         return ({ price: acc.price + curr.price })
-    });
+    }, {price: 0});
     this.cartData = {count : this.finalCount.count, price: this.finalPrice.price}
   }
 
